a45b46ccdb14cb1e3bdf0f3a4124b88b23e7cb57
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-05-25 13:17:43 -0400

    primer programa
    esto es un programa basico en c# que pide al usuario su nombre, apellido, edad y altura, y luego muestra un mensaje con esa información.

---
+++ programa basico/programa basico/Program.cs
@@ -0,0 +1,22 @@
+﻿// esto es un programa basico en c# que pide al usuario su nombre, apellido, edad y altura, y luego muestra un mensaje con esa información.
+//
+
+
+Console.WriteLine("llene los siguientes datos:");
+Console.WriteLine("===============================================");
+Console.Write("Nombre: ");
+string nombre = Console.ReadLine();
+Console.Write("Apellido:");
+string apellido = Console.ReadLine();
+Console.Write("Edad: ");
+int edad = int.Parse(Console.ReadLine());
+Console.Write("Altura: ");
+float altura = float.Parse(Console.ReadLine());
+Console.WriteLine("===============================================");
+
+Console.WriteLine($"Hola mi es {nombre} {apellido}, tengo {edad} años y mido {altura} metros.");
+
+
+
+
+Console. ReadKey();
