c9c56cc5b65c97a37f44d3fe757cd25367f1ff75
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-06-01 22:19:47 -0400

    tabla-de-amortizacion-
    Este proyecto consiste en una aplicación de consola desarrollada en C# utilizando la librería Spectre.Console. La aplicación permite calcular y mostrar una tabla de amortización a partir de los datos proporcionados por el usuario.
    
    El programa solicita información básica del préstamo, calcula la cuota mensual fija y genera una tabla detallada que muestra el comportamiento del préstamo durante todo su plazo.

--- programa basico/programa basico/obj/Debug/net10.0/programa basico.AssemblyInfo.cs
+++ programa basico/programa basico/obj/Debug/net10.0/programa basico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programa basico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ec5a8b8875a3b3cd3480d26711ce782734b7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("programa basico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programa basico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
